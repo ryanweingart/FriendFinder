07863d8178a48118acb5082181686df1fb758abf
Ryan Weingart <rmweingart5@gmail.com>
2019-09-02 00:10:53 -0400

    added data into friends js

---
+++ app/data/friends.js
@@ -0,0 +1,53 @@
+var friends = [
+
+{
+    name: "Ahmed",
+    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  },
+  {
+    name: "Steve",
+    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [
+        4,
+        3,
+        4,
+        1,
+        5,
+        2,
+        5,
+        3,
+        4,
+        1
+      ]
+  },
+  {
+    name: "Mary",
+    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [
+        4,
+        4,
+        2,
+        3,
+        2,
+        2,
+        3,
+        2,
+        4,
+        5
+      ]
+  }
+];
+
+module.exports = friends;
